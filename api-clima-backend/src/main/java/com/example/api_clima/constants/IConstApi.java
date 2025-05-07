38665a4b96c02ef4e33a73bfd9457562fa2427fe
cauaandradevieira <cauaandradevieiraa@gmail.com>
2025-05-07 20:08:41 -0300

    adicionado a contagem de obj que vai vim da api

--- api-clima-backend/src/main/java/com/example/api_clima/constants/IConstApi.java
+++ api-clima-backend/src/main/java/com/example/api_clima/constants/IConstApi.java
@@ -2,6 +2,6 @@ package com.example.api_clima.constants;
 
 public interface IConstApi
 {
-    String URL = "https://api.openweathermap.org/data/2.5/weather?q=%s&units=metric&lang=pt";
+    String URL = "https://api.openweathermap.org/data/2.5/weather?q=%s&units=metric&lang=pt&cont=3";
     String KEY = "&appid=dbb878e504618c0764dbc0663f7b97b8";
 }
